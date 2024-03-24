1bae5be4b0793c677adf4a3eb0c901930c4bdfbd
NeilBardhan <indraneil.bardhan@gmail.com>
2024-03-24 10:36:14 -0700

    Added requirements.txt and additional tests

--- src/demo_py_pkg/stats.py
+++ src/demo_py_pkg/stats.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def mean(input_array: list) -> np.array:
-    return np.mean(np.array(input_array))
+def mean(input_array: list) -> np.float64:
+    return np.mean(input_array)
 
-def median(input_array: list) -> np.array:
-    return np.median(np.array(input_array))
+def median(input_array: list) -> np.float64:
+    return np.median(input_array)
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -5,3 +5,13 @@ from demo_py_pkg import stats
 def test_stats_exists():
     exists = importlib.util.find_spec("demo_py_pkg.stats") is not None
     assert exists
+
+def test_mean():
+    input_list = [4, 5, 6]
+    mean=stats.mean(input_list)
+    assert mean == 5
+
+def test_median():
+    input_list = [4, 5, 6]
+    median=stats.median(input_list)
+    assert median == 5
