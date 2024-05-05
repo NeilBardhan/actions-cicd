7de9aa0099dc1ca85a88e96ff39f4f05d8e3e0b7
Neil Bardhan <neilbardhan@Neils-MacBook-Pro.local>
2024-05-05 10:38:07 -0700

    Added the environment variables to the workflow env setup

--- src/demo_py_pkg/case_tracker.py
+++ src/demo_py_pkg/case_tracker.py
@@ -2,14 +2,14 @@ import os
 import json
 import requests
 from pprint import pprint
-from secrets import CONSUMER_KEY, CONSUMER_KEY_SECRET, ACCESS_TOKEN_URL, CASE_URL, CASE_NUMBER_TEST
+from secrets import CASE_URL, ACCESS_TOKEN_URL
 
 
 def get_access_token():
     data = {
     'grant_type': 'client_credentials',
-    'client_id': os.environ['CONSUMER_KEY'],
-    'client_secret': os.environ['CONSUMER_KEY_SECRET'],
+    'client_id': os.environ['CLIENT_ID'],
+    'client_secret': os.environ['CLIENT_ID_SECRET'],
     }
     response = requests.post(os.environ['ACCESS_TOKEN_URL'], data=data)
     if response.status_code == 200:
@@ -30,7 +30,7 @@ def get_case_status():
         'Authorization': 'Bearer {}'.format(os.environ['USCIS_ACCESS_TOKEN']),
         'Accept': 'application/json'
     }
-    response = requests.get(CASE_URL.format(os.environ['CASE_NUMBER_TEST']), headers=header)
+    response = requests.get(CASE_URL.format(os.environ['TEST_CASE_NUMBER']), headers=header)
     if response.status_code == 200:
         print('Received case status')
         text_lines = response.text.split('\n')
@@ -50,6 +50,6 @@ if __name__ == '__main__':
         os.environ['USCIS_ACCESS_TOKEN'] = access_token
         case_status = get_case_status()
         if case_status is not None:
-            print("CASE STATUS FOR {receipt}: {status}".format(receipt=os.environ['CASE_NUMBER_TEST'], status=case_status))
+            print("CASE STATUS FOR {receipt}: {status}".format(receipt=os.environ['TEST_CASE_NUMBER'], status=case_status))
     else:
         print("failed to get case status")
